8291577f5c2dceba345f8b9535ebc67babf82137
Max Metral <max@pyralis.com>
2023-01-27 22:02:21 -0500

    Add Firebase for crash detection and analytics

--- src/screens/Nav.tsx
+++ src/screens/Nav.tsx
@@ -1,4 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native';
+import analytics from '@react-native-firebase/analytics';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 
@@ -10,24 +11,45 @@ import { SettingsButton } from '../components/SettingsButton';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export const Navigation = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Intro" component={IntroScreen} options={{ headerShown: false }} />
-      <Stack.Screen
-        name="Game"
-        component={GameScreen}
-        options={{
-          headerTitle: '',
-          headerBackTitleVisible: false,
-          headerBackVisible: false,
-          headerShadowVisible: false,
-          headerRight() {
-            return <SettingsButton />;
-          },
-        }}
-      />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+export const Navigation = () => {
+  const routeNameRef = React.useRef<string>();
+  const navigationRef = useNavigationContainerRef();
+
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}>
+      <Stack.Navigator>
+        <Stack.Screen name="Intro" component={IntroScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Game"
+          component={GameScreen}
+          options={{
+            headerTitle: '',
+            headerBackTitleVisible: false,
+            headerBackVisible: false,
+            headerShadowVisible: false,
+            headerRight() {
+              return <SettingsButton />;
+            },
+          }}
+        />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
