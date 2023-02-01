3baa7667101d6c052b603219624b7b1fd4ac48ce
Max Metral <max@pyralis.com>
2023-02-01 10:03:28 -0500

    Fix legendapp state on web

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,22 @@ const path = require('path');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  config.module.rules.push({
-    test: /\.ttf$/,
-    loader: 'url-loader', // or directly file-loader
-    include: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
-  });
-  config.module.rules.push({
-    test: /\.mjs$/,
-    exclude: [/node_modules\/@legendapp\/state\/persist-plugins\/mmkv/],
-  });
+  config.module.rules.push(
+    {
+      test: /\.ttf$/,
+      loader: 'url-loader', // or directly file-loader
+      include: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
+    },
+    {
+      test: /\.mjs$/,
+      exclude: [/node_modules\/@legendapp\/state\/persist-plugins\/mmkv/],
+    },
+    {
+      test: /\.mjs$/,
+      include: /node_modules\/@legendapp/,
+      type: 'javascript/auto',
+    },
+  );
 
   config.resolve.alias = config.resolve.alias || {};
   config.resolve.alias['react-native-svg'] = 'react-native-svg-web';
